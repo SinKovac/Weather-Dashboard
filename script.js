bd88b05caf26d130bb815ea5f1fff7f7e3972c0b
Sin Kovac <ian.kovach@gmail.com>
2020-11-12 17:20:15 -0500

    added final function

--- script.js
+++ script.js
@@ -138,3 +138,19 @@ $.ajax({
         })
     }   
 }
+
+function createForecastCard(date, icon, temp, humidity) {
+
+    let fiveDayCardEl = $("<div>").attr("class", "five-day-card");
+    let cardDate = $("<h3>").attr("class", "card-text");
+    let cardIcon = $("<img>").attr("class", "weatherIcon");
+    let cardTemp = $("<p>").attr("class", "card-text");
+    let cardHumidity = $("<p>").attr("class", "card-text");
+
+    cardRow.append(fiveDayCardEl);
+    cardDate.text(date);
+    cardIcon.attr("src", icon);
+    cardTemp.text(`Temp: ${temp} °F`);
+    cardHumidity.text(`Humidity: ${humidity}%`);
+    fiveDayCardEl.append(cardDate, cardIcon, cardTemp, cardHumidity);
+}
