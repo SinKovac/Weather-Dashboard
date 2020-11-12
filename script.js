a8dd51715ef75777e70300e76b43ae41e2ca04bb
Sin Kovac <ian.kovach@gmail.com>
2020-11-12 15:21:04 -0500

    updated script

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 const APIKey = "c9a9ed03a355403f4cb9a36e931c0b4a";
 
-var searchBtn = $(".searchBtn");
-var searchInput = $(".searchInput");
+var inputEl = document.getElementById("city-input");
+var searchEl = document.getElementById("search-button");
+var nameEl = document.getElementById("city-name");
+var TempEl = document.getElementById("temperature");
+var HumidityEl = document.getElementById("humidity");4
+var WindEl = document.getElementById("wind-speed");
+var UVEl = document.getElementById("UV-index");
+var historyEl = document.getElementById("history");
+    
+var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+console.log(searchHistory);
 
-searchBtn.on("click", function(e) {
-    e.preventDefault();
-    if (searchInput.val() === "") {
-        alert("You must enter a city");
-        return;
-    }
-    console.log("clicked button")
-    getWeather(searchInput.val());
-});
+searchEl.addEventListener("click",function() {
+    const searchTerm = inputEl.value;
+    getWeather(searchTerm);
+    searchHistory.push(searchTerm);
+    localStorage.setItem("search",JSON.stringify(searchHistory));
+    renderSearchHistory();
+})
+
 
