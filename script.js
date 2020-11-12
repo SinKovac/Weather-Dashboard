bbe00ff904ebc2c2b501d20802222592d94beac7
Sin Kovac <ian.kovach@gmail.com>
2020-11-12 17:12:50 -0500

    updated script
    re-did script

--- script.js
+++ script.js
@@ -1,40 +1,39 @@
-const APIKey = "c9a9ed03a355403f4cb9a36e931c0b4a";
+var APIKey = "c9a9ed03a355403f4cb9a36e931c0b4a";
 
-var inputEl = document.getElementById("city-input");
-var searchEl = document.getElementById("search-button");
-var nameEl = document.getElementById("city-name");
-var TempEl = document.getElementById("temperature");
-var HumidityEl = document.getElementById("humidity");4
-var WindEl = document.getElementById("wind-speed");
-var UVEl = document.getElementById("UV-index");
-var historyEl = document.getElementById("history");
-    
-var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
-console.log(searchHistory);
+let searchBtn = $(".searchBtn");
+let searchInput = $(".searchInput");
 
-searchEl.addEventListener("click",function() {
-    const searchTerm = inputEl.value;
-    getWeather(searchTerm);
-    searchHistory.push(searchTerm);
-    localStorage.setItem("search",JSON.stringify(searchHistory));
+let cityNameEl = $(".cityName");
+let currentDateEl = $(".currentDate");
+let weatherIconEl = $(".weatherIcon");
+let searchHistoryEl = $(".historyItems");
+
+let tempEl = $(".temp");
+let humidityEl = $(".humidity");
+let windSpeedEl = $(".windSpeed");
+let uvIndexEl = $(".uvIndex");
+let cardRow = $(".card-row");
+
+var today = new Date();
+let dd = String(today.getDate()).padStart(2, '0');
+let mm = String(today.getMonth() + 1).padStart(2, '0');
+let yyyy = today.getFullYear();
+var today = mm + '/' + dd + '/' + yyyy;
+
+if (JSON.parse(localStorage.getItem("searchHistory")) === null) {
+    console.log("searchHistory not found")
+}else{
+    console.log("searchHistory loaded into searchHistoryArr");
     renderSearchHistory();
-})
-
-function getCityWeather(userInput){
-    displayInfo.innerHTML = '';
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputEl}&units=metric&appid=${APIKey}`)
-    .then(response => response.json())
-    .then(({ main: { temp, humidity }, wind: { speed }, coord: { lon, lat } }) => {
-      var info = document.createElement('div');
-  
-      info.innerHTML = `<h2>${userInput} ${moment().format('MM/DD/YYYY')}</h2>
-      <p>Temperature: ${temp} ºC </p>
-      <p>Humidity: ${humidity}</p>
-      <p>Wind Speed: ${speed} mph </p>
-      `
-        displayInfo.append(info)
-        getUvIndex(lon, lat)
-        getFiveDayForecast(lon, lat)
-        
-    })
 }
+
+searchBtn.on("click", function(e) {
+    e.preventDefault();
+    if (searchInput.val() === "") {
+        alert("You must enter a city");
+        return;
+    }
+    console.log("clicked button")
+    getWeather(searchInput.val());
+});
+
