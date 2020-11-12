bd554106f643daa98936c6815e167e90c461f17f
Sin Kovac <ian.kovach@gmail.com>
2020-11-12 15:00:07 -0500

    updated script
    search button variables and function

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 const APIKey = "c9a9ed03a355403f4cb9a36e931c0b4a";
 
+var searchBtn = $(".searchBtn");
+var searchInput = $(".searchInput");
+
+searchBtn.on("click", function(e) {
+    e.preventDefault();
+    if (searchInput.val() === "") {
+        alert("You must enter a city");
+        return;
+    }
+    console.log("clicked button")
+    getWeather(searchInput.val());
+});
+
